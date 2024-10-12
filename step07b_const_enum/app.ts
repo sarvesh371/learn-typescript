90ac8ed682e62b72dd9ae145d485b1059bf26efa
sarvesh371 <sarveshsingh.03@gmail.com>
2024-10-12 14:03:29 +0530

    const enums

--- step07b_const_enum/app.ts
+++ step07b_const_enum/app.ts
@@ -1,8 +1,8 @@
 const enum Color {Red, Green, Blue};//starts with 0
 var c: Color = Color.Green;
 
-const enum Color1 {Red = 1, Green, Blue};
-var colorName: string = Color[2]; //Not allowed with const enums
+enum Color1 {Red = 1, Green, Blue};
+var colorName: string = Color1[2]; // Allowed with regular enums
 console.log(colorName);
 
 const enum Color2 {Red = 1, Green = 2, Blue = 4};//can assign values to all
